1787d4be880e61941655587340a0d587f6d99d24
aragorndev0912 <aragorn.dev0912@gmail.com>
2021-01-24 13:01:01 +0100

    title changed

--- config.h
+++ config.h
@@ -4,6 +4,6 @@
 #define SCREEN_WIDTH    500
 #define SCREEN_HEIGHT   500
 #define SCREEN_FPS      60
-#define SCREEN_TITLE    "RayCasting"
+#define SCREEN_TITLE    "Dungeon"
 
 #endif /* RC_CONFIG_H */
